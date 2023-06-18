c7b73e1bcfbf58b54394b2bf4da7ced3f92036f5
Serge3452 <serge3453@gmail.com>
2023-06-18 22:55:00 +0500

    Третья загрузка
    Третья загрузка

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
-console.log("Hello World Группа")
-console.log(666);
-console.log("Hello" + " World");
+//console.log("Hello World Группа")
+//console.log(666);
+//console.log("Hello" + " World");
+
+// let a = 1;
+// console.log(1 + "qwe");
+// тип данных Number
+// спец значения NaN Infinity
+
+// Bigint
+
+// console.log(98n);
+
+// sring строка
+
+// let a = "Anime ";
+// let b = "JOjO";
+// let c = `Naruto ${a + b}`;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// boolean логические
+// let DotCheck = true;
+// let DockUnCheck = false;
+// console.log(DockUnCheck);
+// console.log(DotCheck);
+
+// console.log(1 < 2);
+
+// Null
+// let nuller = null;
+// console.log(nuller);
+
+// undefined
+// let a = undefined;
+// console.log(a);
+
+//
+
+// console.log(typeof 20);
+// console.log(typeof null);
+document.querySelector(".qwe").innerHTML = "Это наш первый вывод по средством JS";
+
+console.log (4) // месяц моего рождения
+console.log('Добро '+'пожаловать '+' на курс ' + ' Сергей') // вывод приветствия
+
+let date = new Date // вывод текуще  даты и времени
+alert( date);
+
+document.getElementById("one").innerHTML= "Hello World"
